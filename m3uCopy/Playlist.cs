1a1c799bf06d7242cd3765aa9a0a638caa1cc3e5
draakon <draakon@masendav.org>
2013-07-29 21:01:57 +0300

    Initial commit: first version.

---
+++ m3uCopy/Playlist.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace m3uCopy  {
+    class Playlist : Queue<string[]> {
+        private string playlist;
+        private string destination;
+
+        public Playlist(string playlist, string destination) {
+            this.playlist = playlist;
+            this.destination = destination;
+
+            StreamReader reader = new StreamReader(playlist);
+            string file;
+            while ((file = reader.ReadLine()) != null) {
+                this.Enqueue(file);
+            }
+        }
+
+        public void Enqueue(string file) {
+            if (!File.Exists(file)) {
+                Console.Error.WriteLine("File '{0}' doesn't exist! Skipping.", file);
+                return;
+            }
+            string filename = Path.GetFileName(file);
+            string destFile = Path.Combine(destination, filename);
+            if (File.Exists(destFile)) {
+                Console.Error.WriteLine("File '{0}' already exists at destination! Skipping.", filename);
+                return;
+            }
+
+            base.Enqueue(new string[2] {file, destFile});
+        }
+
+        public void CopyFiles() {
+            string[] item;
+            while (this.Count > 0) {
+                item = this.Dequeue();
+                Console.WriteLine("Copying: '{0}'", Path.GetFileName(item[0]));
+                try {
+                    File.Copy(item[0], item[1], false);
+                } catch (Exception e) {
+                    Console.Error.WriteLine("Copying file '{0}' failed!\nError message: '{1}'. Skipping.", item[0], e.ToString());
+                }
+      
+            }
+        }
+
+    }
+}
---
+++ m3uCopy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace m3uCopy {
+    class Program  {
+        private static Playlist playlist;
+
+        static void Main(string[] args) {
+            if (args.Length != 2) Program.ExitWithError("m3uCopy takes 2 arguments.");
+                
+            string playlist = args[0];
+            string destination = args[1];
+
+            if (!File.Exists(playlist)) Program.ExitWithError("PLAYLIST FILE doesn't exists!");
+            if (!Directory.Exists(destination)) Program.ExitWithError("OUTPUT DIRECTORY doesn't exist!");
+
+            Console.WriteLine("Enqueuing files...");
+            Program.playlist = new Playlist(playlist, destination);
+
+            Console.WriteLine("{0} {1} in queue.\nCopying...", Program.playlist.Count, (Program.playlist.Count == 1 ? "file" : "files"));
+            Program.playlist.CopyFiles();
+            Console.WriteLine("Done.");
+        }
+
+        static void ExitWithError(String text, int exitCode = 1) {
+            Console.Error.WriteLine(text);
+            Console.WriteLine();
+            Program.WriteHelp();
+            Environment.Exit(exitCode);
+        }
+
+        static void WriteHelp() {
+            Console.WriteLine("Usage: m3uCopy [PLAYLIST FILE] [OUTPUT DIRECTORY]");
+            Console.WriteLine("Copies files from PLAYLIST FILE to OUTPUT DIRECTORY.");
+        }
+    }
+}
